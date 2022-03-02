0158e4abc54f870c965ce2ccb6183b0042d04fa6
Alex C <cxz980314@gmail.com>
2022-03-02 10:36:45 -0500

    adding models

---
+++ models/comments.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+// id, user_id, wine_id, comment
+Comment.init(
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      },
+      wine_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'wine',
+          key: 'id'
+        }
+      },
+      comment: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
+);
+
+module.exports = Comment;
---
+++ models/index.js
@@ -0,0 +1,30 @@
+const User = require('./user');
+const Wine = require('./wine');
+const Comment = require('./comment');
+
+User.hasMany(Wine, {
+    foreignKey: 'user_id'
+});
+
+Wine.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+Comment.belongsTo(Wine, {
+    foreignKey: 'Wine_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+  
+Wine.hasMany(Comment, {
+    foreignKey: 'Wine_id'
+});
+
+module.exports = {User, Wine, Comment};
