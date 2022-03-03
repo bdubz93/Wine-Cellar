8e2dd79ba3d5260ee53deb02c5989651364457f3
Brandon Winkler <brandonwinkler@live.ca>
2022-03-02 21:49:58 -0500

    deleted old package code

--- server.js
+++ server.js
@@ -31,31 +31,6 @@ const sess = {
 app.use(session(sess));
 
 
-app.use(fileUpload());
-
-app.post('/upload', async (req, res) => {
-  const {name, data} = req.files.pic;
-  if (name && data) {
-      await sequelize.create({name: name, img: data}).into('img');
-      res.sendStatus(200);
-  } else {
-      res.sendStatus(400);
-  }
-})
-
-app.get('/img/:id', async (req, res) => {
-  const id = req.params.id;
-  const img = await sequelize('img').where({id: id}).first();
-  if (img) {
-      const contentType = await FileType.fromBuffer(img.img); // get the mimetype of the buffer (in this case its gonna be jpg but can be png or w/e)
-      res.type(contentType.mime); // not always needed most modern browsers including chrome will understand it is an img without this
-      res.end(img.img);
-  } else {
-      res.end('No Img with that Id!');
-  }
-})
-
-
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
