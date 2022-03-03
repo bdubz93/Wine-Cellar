f6cf316e79d19b588646889769e9a1c385545ac6
Brandon Winkler <brandonwinkler@live.ca>
2022-03-03 13:47:56 -0500

    comments are posting, need get route to see where

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
 const collectionRoutes = require('./collectionRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
 router.use('/collection', collectionRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
